7d16f64723b29b424a6f8693b6542f5218d6e206
huangyh45 <116069282+huangyh45@users.noreply.github.com>
2024-03-28 06:24:17 +0800

    Update run_commands.sh

--- plastome/run_commands.sh
+++ plastome/run_commands.sh
@@ -9,7 +9,7 @@ done
 PhyloAln/scripts/select_seqs.py test Piper_sp._2_Munzinger_6536,Piper_borbonense_1_NCY_873616,Piper_borbonense_2_Ramahefaharivelo_191,Piper_caninum_1_Smith_6545,Piper_caninum_2_Gray_9430,Piper_hederaceum_Gray_9456,Piper_insectifugum_Munzinger_6571,Piper_mestonii_Gray_9458,Piper_sarmentosum_Smith_s.n.1,Piper_sp._3_Davidson_12685,Piper_commutatum_Angel_2356,Piper_darienense_Aguilar_11197,Piper_umbellatum_Davidson_12352,Piper_capense_1_Davidson_11009,Piper_capense_2_Smith_4925,Piper_guahamense_Flynn_6748 ref .fa . 1
 
 # run PhyloAln
-time /public/lihaosen/PhyloAln/PhyloAln -d ref -c plastid.config -f fastq -p 20 -m codon -g 11 -b -u Magnolia_grandiflora -r
+time PhyloAln/PhyloAln -d ref -c plastid.config -f fastq -p 20 -m codon -g 11 -b -u Magnolia_grandiflora -r
 
 # calculate the percent completeness and identity of PhyloAln results
 PhyloAln/scripts/test_effect.py test:Piper_sp._2_Munzinger_6536 PhyloAln_out/nt_out:Piper_sp2 Piper_sp2.plastome.tsv N . .fa Drimys_granadensis,Aristolochia_contorta,Aristolochia_debilis,Magnolia_grandiflora,Piper_auritum,Piper_cenocladum,Piper_kadsura,Piper_laetispicum,Piper_nigrum,Saruma_henryi,Peperomia_berteroana_JBN_3340,Peperomia_congesta_HW_6593,Peperomia_fernandopoiana_Razoazanany_97,Peperomia_glabella_Stevens_34572,Peperomia_margaritifera_JBN_3342,Peperomia_tetraphylla_Lim_74,Piper_ponapense_Wood_13509,Piper_puberulum_Lorence_10610,Piper_sp._1_McPherson_19040
